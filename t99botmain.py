21d6a2c319f1d78bef71252ae5ff64325ad93f74
Mango0x45 <57815710+Mango0x45@users.noreply.github.com>
2019-12-30 00:43:51 +0100

    Add files via upload

--- t99botmain.py
+++ t99botmain.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
 import datetime
+import json
 
-client = commands.Bot(command_prefix = ':')
+client = commands.Bot(command_prefix = ';')
 tokenFile = open('token.txt', 'r')
 token = tokenFile.readline()
 
 @client.event
 async def on_ready():
-    print(f'Online: Tetris99 Bot#7917 (ID: 651135979209621516)')
+    print('Online: Tetris99 Bot#7917 (ID: 651135979209621516)')
 
 @client.event
 async def on_member_join(member):
@@ -19,9 +20,28 @@ async def on_member_join(member):
     userSinceCreated = (dateNow - dateCreated).days * 24
 
     if userSinceCreated > 48:
-        print(userSinceCreated)
+        #print(userSinceCreated)
+        pass
     else:
         await t99Server.ban(user=member, reason="Account is less than 48 hours old")
         await modChannel.send(memberMention + '** has been kicked from the server for having an account under 48 hours old**')
+
+@client.command(name = 'ping')
+async def ping(ctx):
+    await ctx.send(f'{ctx.author.mention} Pong!')
+
+@client.command(name = 'leaderboard')
+async def leaderboard(ctx, numberOfPlayers = 10):
+    with open('leaderboard.json', 'r+') as f:
+        leaderboardFile = json.load(f)
+
+        sortedDict = {k: v for k, v in sorted(leaderboardFile.items(), key=lambda item: item[1], reverse=True)}
+        counter = 1
+        fullMessage = ''
+
+        for player in sortedDict:
+            fullMessage += f'{player} - {sortedDict[player]}★\n'
+
+        await ctx.send(fullMessage)
 
 client.run(token.strip())
